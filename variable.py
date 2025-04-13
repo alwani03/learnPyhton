7384ce31bb13867be602680fe75e8d434a719259
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-04-13 18:46:16 +0700

    #01 Variabel

---
+++ variable.py
@@ -0,0 +1,16 @@
+# Variable
+nama = "alwani"
+# print(nama)
+
+umur = 25
+# print(umur)
+
+## Penulisan sekaligus
+newName, address, gender = 'Achmad Alwani', 'Street Raya Raya', 'Male' 
+print('Name    :', newName)
+print('Address :', address)
+print('Gender  :', gender)
+
+# Cara Pengecekan variable
+
+print('Variable ini Bertipe : ',type(newName))
