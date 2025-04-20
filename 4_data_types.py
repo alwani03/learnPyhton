c0ac7a9ace05bf106b83d90b0a9c505794defc14
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-04-20 11:15:19 +0700

    4.Data types

---
+++ 4_data_types.py
@@ -0,0 +1,107 @@
+# str 
+x = "Hello World"
+
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = 20
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = 20.5
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+x = 1j
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+print("|----------------------|")
+print("|                      |")
+
+
+
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = range(6)
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = True
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = b"Hello"
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = bytearray(5)
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = memoryview(bytes(5))
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
+x = None
+print(x)
+print(type(x)) 
+print("|----------------------|")
+print("|                      |")
+
+
