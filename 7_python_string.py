ac56d6340566f5c8ef49cf4de8e42d1c1b8b06e8
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-05-15 20:00:23 +0700

    7 python string

---
+++ 7_python_string.py
@@ -0,0 +1,32 @@
+print("Hello")
+print('Hello')
+
+print('------------------------')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+print('------------------------')
+
+
+a = "Hello"
+print(a)
+
+print('------------------------')
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+print('------------------------')
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
